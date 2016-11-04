98f69e265233d3663ef4604819d5c6a7a39fc3c8
max02man <max02man@gmail.com>
2016-11-04 01:50:37 -0400

    CIS199 program 1
    calculate paint job for a company

---
+++ program1/program1/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace program1
+{
+    partial class program1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculate_button = new System.Windows.Forms.Button();
+            this.Exit_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Square_Feet_textBox = new System.Windows.Forms.TextBox();
+            this.Gallon_label = new System.Windows.Forms.Label();
+            this.Time_label = new System.Windows.Forms.Label();
+            this.Labor_label = new System.Windows.Forms.Label();
+            this.Total_label = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gallon_textBox = new System.Windows.Forms.TextBox();
+            this.coats_textBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.squarefeet_label = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pricegallon_label = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Calculate_button
+            // 
+            this.Calculate_button.Location = new System.Drawing.Point(118, 116);
+            this.Calculate_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Calculate_button.Name = "Calculate_button";
+            this.Calculate_button.Size = new System.Drawing.Size(86, 32);
+            this.Calculate_button.TabIndex = 4;
+            this.Calculate_button.Text = "Calculate";
+            this.Calculate_button.UseVisualStyleBackColor = true;
+            this.Calculate_button.Click += new System.EventHandler(this.Calculate_button_Click);
+            // 
+            // Exit_button
+            // 
+            this.Exit_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Exit_button.Location = new System.Drawing.Point(16, 116);
+            this.Exit_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Exit_button.Name = "Exit_button";
+            this.Exit_button.Size = new System.Drawing.Size(86, 32);
+            this.Exit_button.TabIndex = 5;
+            this.Exit_button.Text = "Exit";
+            this.Exit_button.UseVisualStyleBackColor = true;
+            this.Exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Space by sq ft";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Gallon Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 66);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hours of Labor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 126);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Labor Cost";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Total Cost";
+            // 
+            // Square_Feet_textBox
+            // 
+            this.Square_Feet_textBox.Location = new System.Drawing.Point(93, 18);
+            this.Square_Feet_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Square_Feet_textBox.Name = "Square_Feet_textBox";
+            this.Square_Feet_textBox.Size = new System.Drawing.Size(86, 20);
+            this.Square_Feet_textBox.TabIndex = 1;
+            // 
+            // Gallon_label
+            // 
+            this.Gallon_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Gallon_label.Location = new System.Drawing.Point(120, 39);
+            this.Gallon_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Gallon_label.Name = "Gallon_label";
+            this.Gallon_label.Size = new System.Drawing.Size(78, 17);
+            this.Gallon_label.TabIndex = 8;
+            this.Gallon_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Time_label
+            // 
+            this.Time_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Time_label.Location = new System.Drawing.Point(120, 65);
+            this.Time_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Time_label.Name = "Time_label";
+            this.Time_label.Size = new System.Drawing.Size(78, 17);
+            this.Time_label.TabIndex = 9;
+            this.Time_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Labor_label
+            // 
+            this.Labor_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Labor_label.Location = new System.Drawing.Point(120, 126);
+            this.Labor_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Labor_label.Name = "Labor_label";
+            this.Labor_label.Size = new System.Drawing.Size(78, 17);
+            this.Labor_label.TabIndex = 10;
+            this.Labor_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Total_label
+            // 
+            this.Total_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Total_label.Location = new System.Drawing.Point(120, 152);
+            this.Total_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Total_label.Name = "Total_label";
+            this.Total_label.Size = new System.Drawing.Size(78, 17);
+            this.Total_label.TabIndex = 11;
+            this.Total_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 40);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Gallon";
+            // 
+            // gallon_textBox
+            // 
+            this.gallon_textBox.Location = new System.Drawing.Point(93, 44);
+            this.gallon_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gallon_textBox.Name = "gallon_textBox";
+            this.gallon_textBox.Size = new System.Drawing.Size(86, 20);
+            this.gallon_textBox.TabIndex = 2;
+            // 
+            // coats_textBox
+            // 
+            this.coats_textBox.Location = new System.Drawing.Point(93, 70);
+            this.coats_textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.coats_textBox.Name = "coats_textBox";
+            this.coats_textBox.Size = new System.Drawing.Size(86, 20);
+            this.coats_textBox.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 73);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Coats";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 17);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Total Square Feet";
+            // 
+            // squarefeet_label
+            // 
+            this.squarefeet_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.squarefeet_label.Location = new System.Drawing.Point(120, 17);
+            this.squarefeet_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.squarefeet_label.Name = "squarefeet_label";
+            this.squarefeet_label.Size = new System.Drawing.Size(78, 17);
+            this.squarefeet_label.TabIndex = 17;
+            this.squarefeet_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pricegallon_label);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.Total_label);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.Gallon_label);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.squarefeet_label);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.Labor_label);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Time_label);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(9, 153);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(202, 185);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Order";
+            // 
+            // pricegallon_label
+            // 
+            this.pricegallon_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pricegallon_label.Location = new System.Drawing.Point(120, 98);
+            this.pricegallon_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.pricegallon_label.Name = "pricegallon_label";
+            this.pricegallon_label.Size = new System.Drawing.Size(78, 17);
+            this.pricegallon_label.TabIndex = 23;
+            this.pricegallon_label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(5, 99);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Paint Cost";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.Square_Feet_textBox);
+            this.groupBox2.Controls.Add(this.coats_textBox);
+            this.groupBox2.Controls.Add(this.gallon_textBox);
+            this.groupBox2.Location = new System.Drawing.Point(9, 10);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(202, 102);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Enter Your Order";
+            // 
+            // program1
+            // 
+            this.AcceptButton = this.Calculate_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.Exit_button;
+            this.ClientSize = new System.Drawing.Size(221, 346);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Exit_button);
+            this.Controls.Add(this.Calculate_button);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "program1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Painting Company ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Calculate_button;
+        private System.Windows.Forms.Button Exit_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Square_Feet_textBox;
+        private System.Windows.Forms.Label Gallon_label;
+        private System.Windows.Forms.Label Time_label;
+        private System.Windows.Forms.Label Labor_label;
+        private System.Windows.Forms.Label Total_label;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox gallon_textBox;
+        private System.Windows.Forms.TextBox coats_textBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label squarefeet_label;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label pricegallon_label;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ program1/program1/Form1.cs
@@ -0,0 +1,69 @@
+﻿//Alaa Hawsawi
+//Program1
+//due date 10/1/2014
+//CIS199-01
+//the program will calculate the costoumer order 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program1
+{
+    public partial class program1 : Form
+    {
+      
+        public program1()
+        {        
+            InitializeComponent();
+        }
+
+        private void Calculate_button_Click(object sender, EventArgs e)
+        {
+            float space;
+            int coats;
+            float gallonprice;
+            double labor = 10.50;
+            double gallon;           
+            double time = 8;
+            double total;
+            double sqft = 325;
+
+          
+            //input
+           space= float.Parse(Square_Feet_textBox.Text);       //input how many space
+           coats = int.Parse(coats_textBox.Text);       //input coats
+           gallonprice = float.Parse(gallon_textBox.Text); //input the gallon price
+
+           //Calculate
+           double sqftperhr = sqft / time; //HOW MANY SQUARE FEET BY HOW MANY FEET PER HOUR
+           space = space * coats;         //cal how many square feet with coats
+           gallon = space / sqft;      //cal give how many gallons need 
+           gallon = Math.Ceiling(gallon);      //round how many gallans the need
+           gallonprice = (float)gallon * gallonprice;
+           time =  space/sqftperhr;   //cal how many hour need to finish the paint
+           labor = labor * time;       //cal the cost of labor
+           total = gallonprice + labor; //cal the total of the job 
+
+            //output
+           Gallon_label.Text = gallon.ToString();   //output how many gallons will use.
+           squarefeet_label.Text = space.ToString("n1") +" sq ft";    //output how many sq ft with coat
+           pricegallon_label.Text = gallonprice.ToString("c");  //output of the price of gallon
+           Time_label.Text = time.ToString("F1");   //output how many hours of labor
+           Labor_label.Text = labor.ToString("C");     //output how much cost of labor
+           Total_label.Text = total.ToString("C");     //output how much total of painting 
+        }
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+      
+    }
+}
